571f33cca6ad6d7c6ef6835cc59203328dc5abfd
Jeff Coding Mentor <97670346+JeffCodingMentor@users.noreply.github.com>
2023-01-09 15:41:40 +0800

    疫苗護照顯示姓名、頭像、生日

--- app/src/main/java/com/example/myapplication/PassportActivity.kt
+++ app/src/main/java/com/example/myapplication/PassportActivity.kt
@@ -1,12 +1,16 @@
 package com.example.myapplication
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.*
 import androidx.cardview.widget.CardView
+import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
+import java.io.File
 
 class PassportActivity : AppCompatActivity() {
 
@@ -14,10 +18,14 @@ class PassportActivity : AppCompatActivity() {
     private lateinit var iv1:ImageView
     private lateinit var iv2:ImageView
     private lateinit var iv3:ImageView
+    private lateinit var sharedPref: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_passport)
+        sharedPref = getSharedPreferences("myPref", MODE_PRIVATE)
+
+        initUserInfo()
 
         val btn1 = findViewById<CardView>(R.id.CardViewDoz1)
         val btn2 = findViewById<CardView>(R.id.CardViewDoz2)
@@ -57,6 +65,11 @@ class PassportActivity : AppCompatActivity() {
             val intent = Intent(this, UserInfoActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initUserInfo()
     }
 
     private fun changeColor() {
@@ -125,5 +138,25 @@ class PassportActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+    private fun initUserInfo() {
+        val img_path = sharedPref.getString("imguri","")
+        val chname = sharedPref.getString("chname","")
+        val enname = sharedPref.getString("enname","")
+        val bday = sharedPref.getString("bday","")
+
+        if(img_path != null) {
+            if (img_path!="") {
+                val imgPinfo = findViewById<ImageView>(R.id.imgPersonalInfo)
+                val img_uri = File(img_path).toUri() //要到手機裡開權限
+                imgPinfo.setImageURI(img_uri)
+            }
+        }
+
+        val tv_Name = findViewById<TextView>(R.id.textViewName)
+        tv_Name.text = chname+" "+enname //TODO:中英文名顯示要分開
+        val tv_bday = findViewById<TextView>(R.id.textViewBirthDay)
+        tv_bday.text = bday
     }
 }
